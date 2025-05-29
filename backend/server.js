477d4393eb12930061ff486a1911955b753852d8
Arogunmasa Abayomi <nomorelimit11@gmail.com>
2025-05-29 22:51:02 +0100

    Update dependencies and server configuration
    - Update backend/server.js and .vscode/settings.json
    - Update and add nodemon, nodetouch, semver binaries in node_modules
    - Update mongoose, mongodb, bson, sift, and touch dependencies
    - Update package.json and package-lock.json

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express, { request } from "express";
-import mongoose from "mongoose";
+import express from "express";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -24,8 +24,8 @@ app.get("/api/products", async (request, response) => {
 	}
 });
 
-app.post("/api/products", (request, response) => {
-	const product = request.body;
+app.post("/api/products", async (request, response) => {
+	const product = await request.body;
 	// Here you would typically save the product to the database
 
 	if (
@@ -73,6 +73,42 @@ app.delete("/api/products/:id", async (request, response) => {
 			error: error.message,
 		});
 	}
+});
+
+app.put("/api/products/:id", async (request, response) => {
+	const productID = request.params.id;
+	const product = request.body;
+	console.log(product);
+
+	if (!mongoose.Types.ObjectId.isValid(productID))
+		return response
+			.status(404)
+			.json({ success: false, message: "Product not found" });
+
+	try {
+		const updatedProducts = await Product.findByIdAndUpdate(
+			productID,
+			product,
+			{
+				new: true,
+			}
+		);
+		response.status(200).json({
+			success: true,
+			message: "Product successfully updated",
+			data: updatedProducts,
+		});
+	} catch (error) {
+		response.status(500).json({
+			success: false,
+			message: "Server error",
+			error: error.message,
+		});
+	}
+
+	// const product = await Product.findByIdAndUpdate(productID);
+	// product.name = "Newly Updated Product";
+	// Product.save();
 });
 
 app.get("/", (request, response) => {});
